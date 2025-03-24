42a2e599ec8704dab87a12c80f18ae7a28afae6a
sashavershkova <a.m.vershkova@gmail.com>
2025-03-24 14:36:36 -0700

    diff lines - 27

--- function_a.py
+++ function_a.py
@@ -20,3 +20,8 @@ def most_common_value(number_list):
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
     print(f"Most common value = {most_common_value(nums)}")
+
+
+
+
+    # hello 
